a59eed7f74c9ac362a0358428678944fe5cc38bd
vincentyonathan <vincentyonathan24@gmail.com>
2021-11-21 19:02:09 +0700

    initial

---
+++ ProductManagement/Controllers/ItemsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductManagement.Context;
+
+namespace ProductManagement.Controllers
+{
+    public class ItemsController : Controller
+    {
+        StorageItemDBEntities dbObj = new StorageItemDBEntities();
+        // GET: Items
+        [Authorize]
+        public ActionResult Items(Item obj)
+        {
+            return View(obj);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult AddItems(Item model)
+        {
+
+            if(ModelState.IsValid)
+            {
+                Item obj = new Item();
+                obj.ItemID = model.ItemID;  
+                obj.ItemName = model.ItemName;
+                obj.ItemDesc = model.ItemDesc;
+                obj.ItemSupplier = model.ItemSupplier;
+                obj.ItemBrand = model.ItemBrand;
+                obj.ItemCategory = model.ItemCategory;
+                obj.ItemPrice = model.ItemPrice;
+                obj.ItemStock = model.ItemStock;
+
+                if(model.ItemID == 0)
+                {
+                    dbObj.Items.Add(obj);
+                    dbObj.SaveChanges();
+                }
+                else
+                {
+                    dbObj.Entry(obj).State = EntityState.Modified;
+                    dbObj.SaveChanges();
+                }
+            }
+            ModelState.Clear();
+
+            return View("Items");
+        }
+
+        [Authorize]
+        public ActionResult ItemsList()
+        {
+            var res = dbObj.Items.ToList();
+            return View(res);
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            var res = dbObj.Items.Where(x => x.ItemID == id).FirstOrDefault();
+            dbObj.Items.Remove(res);
+            dbObj.SaveChanges();
+
+            var list = dbObj.Items.ToList();
+
+            return View("ItemsList",list); 
+        }
+
+        [Authorize]
+        public ActionResult Details(Item obj)
+        {
+            return View(obj);
+        }
+    }
+}
---
+++ ProductManagement/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ProductManagement.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string supplier { get; set; }
+        public string brand { get; set; }
+        public string category { get; set; }
+        public float price { get; set; }
+        public int stock { get; set; }
+    }
+}
